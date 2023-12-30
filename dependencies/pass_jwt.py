fc213cf1a4840d26d23ec15b311a27061c4ba345
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2023-12-30 20:50:54 +0530

    login part added

--- dependencies/pass_jwt.py
+++ dependencies/pass_jwt.py
@@ -1,9 +1,21 @@
 from passlib.context import CryptContext
+from jose import JWTError, jwt
+from dependencies.config import SECRET_KEY, ALGORITHM
+from datetime import timedelta, datetime
+
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
 def create_hashed_password(password):
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-
+def create_jwt_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- routers/users.py
+++ routers/users.py
@@ -36,5 +36,14 @@ async def verify_user(user : user.User, otp: str):
     else:
         return response
 
-
-    
+@router.get("/users/login", tags=["User"])
+async def login_user(username: str, password: str):
+    user = mongo.db.users.find_one({"username": username})
+    if user:
+        if pass_jwt.verify_password(password, user["password"]):
+            jwt_token = pass_jwt.create_jwt_token({"username": user["username"]})
+            return {"message": "Login successful", "token": jwt_token, "token_type": "bearer"}
+        else:
+            return JSONResponse(content={"message": "Password is incorrect"}, status_code=400)
+    else:
+        return JSONResponse(content={"message": "Username not found"}, status_code=400)
