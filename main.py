eb7e8acd4cdc917b6d4b5e8583fceefab860efae
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2023-12-30 22:23:38 +0530

    Revert "create event api added"
    This reverts commit f63a6ded25e933b072a1faaa68651583ced43707.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from routers import user, event
+from routers import user
 
 app = FastAPI()
 app.include_router(user.router)
-app.include_router(event.router)
 
 @app.get("/")
 async def root():
--- routers/user.py
+++ routers/user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from models import user
+from model import user
 from dependencies import mongo, email_auth, pass_jwt
 from fastapi.responses import JSONResponse
 
